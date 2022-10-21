84eb45cdb6bf4dee35abecef7f0d854a4402e2d8
samuel_vc <samuel@getxplor.com>
2022-10-21 08:22:39 -0500

    Subiendo archivos

---
+++ src/index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const { autoUpdater } = require('electron')
+const { version } = require('../package.json')
+
+const server = 'https://update.electronjs.org'
+const feed = `${server}/electron/update-server/${process.platform}/${version}`
+
+console.log(`Current version: ${version}`)
+
+autoUpdater.setFeedURL(feed)
+autoUpdater.checkForUpdates()
+
+autoUpdater.on('checking-for-update', () => {
+  console.log('checking-for-update')
+})
+
+autoUpdater.on('update-available', () => {
+  console.log('update-available')
+})
+
+autoUpdater.on('update-not-available', () => {
+  console.log('update-not-available')
+})
+
+autoUpdater.on(
+  'update-downloaded',
+  (event, releaseNotes, releaseName, updateURL) => {
+    console.log('update-downloaded', {
+      event,
+      releaseNotes,
+      releaseName,
+      updateURL
+    })
+  }
+)
+
+autoUpdater.on('error', error => {
+  console.log('error', { error })
+})
